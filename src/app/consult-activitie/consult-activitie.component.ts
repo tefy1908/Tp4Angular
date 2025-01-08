12ad1ba63627da2792062ff027cad4aadc291469
tefy1908 <tefymaherison19@gmail.com>
2025-01-08 11:20:29 +0100

    tp4

--- src/app/consult-activitie/consult-activitie.component.ts
+++ src/app/consult-activitie/consult-activitie.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { TestServiceService } from '../test-service.service';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
